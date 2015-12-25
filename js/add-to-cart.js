8a374ec27f01ce6390c21c1a07ff40bf3333d839
Vadym-Filipov <melkore87@gmail.com>
2015-12-25 13:14:37 +0200

    bin

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -2,11 +2,13 @@
 
     var addpos = document.getElementsByClassName('collection_descr-coast');
     var count = 0;
+    var maxCount = -1;
     var price = 0;
     var price1, name1;
     var priceMas = [];
     var nameMas = [];
     var savedCount = window.localStorage.count;
+    var savedMaxCount = window.localStorage.maxCount;
     var savedPrice = window.localStorage.price;
     var setitems = document.getElementsByClassName('customer_check-quantity')[0];
     var setprice = document.getElementsByClassName('customer_check-summ')[0];
@@ -14,35 +16,38 @@
     if (savedCount) {
         count = savedCount;
         price = savedPrice;
+        maxCount = savedMaxCount
         if (savedCount == 1) {
             setitems.innerHTML = savedCount + ' item';
         }
         else {setitems.innerHTML = savedCount + ' items';}
         setprice.innerHTML = '$ ' + savedPrice;
 
-        for (var j = 1; j<=count; j++) {
+        for (var j = 1; j<=maxCount; j++) {
             price1 = 'price' + j;
             var tempPrice = window.localStorage[price1];
             priceMas.push(tempPrice);
         }
 
-        for (var k = 1; k<=count; k++) {
+        for (var k = 1; k<=maxCount; k++) {
             name1 = 'name' + k;
             var tempName = window.localStorage[name1];
             nameMas.push(tempName);
         }
 
-        for (var l = 0; l<priceMas.length; l++) {
-            $('.place_order').append('<p><span> X</span></p>');
+        for (var l = 0; l<maxCount; l++) {
+            if (nameMas[l] == undefined){continue;}
+            $('.place_order').append('<p class="order_position"><span class="remove_order-position"> X</span></p>');
             $('.place_order>p:last-child').prepend(nameMas[l] + ' ' + priceMas[l]);
         }
-        $( ".place_order>p>span" ).click(removePosition);
+        //$( ".place_order>p>span" ).click(removePosition);
     }
 
     for (var i = 0; i<addpos.length; i++) {
 
         addpos[i].nextSibling.onclick = function () {
             count++;
+            if (count > maxCount){maxCount=count;window.localStorage.maxCount = maxCount;}
             if (count == 1) {setitems.innerHTML = count + ' item';}
                 else {setitems.innerHTML = count + ' items'}
             window.localStorage.count = count;
@@ -62,21 +67,21 @@
             name1 = 'name' + count;
             window.localStorage[price1] = elemprice;
             window.localStorage[name1] = elemName;
-            $('.place_order').append('<p><span> X</span></p>');
+            $('.place_order').append('<p class="order_position"><span class="remove_order-position"> X</span></p>');
             //$('.place_order').append('<p></p>');
             $('.place_order>p:last-child').prepend(elemName + " " + elemprice);
-            $( ".place_order>p>span" ).click(removePosition);
+            //$( ".place_order>p>span:last-child" ).click(removePosition);
         }
     }
 
-
+   // $( ".place_order>p>span" ).click(removePosition);
 
     $( ".customer_check" ).click(function() {
         $( ".place_order" ).toggle( "slow", function(){
         });
     });
 
-    $( ".place_order>p:eq(0)" ).click(function() {
+    $( ".place_button" ).click(function() {
         $( ".place_order" ).toggle( "slow", function(){
         });
     });
@@ -93,7 +98,7 @@
         var pos1 = elempricestr.indexOf(" X");
         var elprice = elempricestr.substring(pos, pos1);
         //var elemName = position.previousSibling.previousElementSibling.textContent;
-        price = window.localStorage.price;
+        //price = window.localStorage.price;
         var subprice = elprice.substring(1, elprice.length);
         parseFloat(subprice);
         parseFloat(price);
@@ -105,15 +110,15 @@
         window.localStorage.price = price;
         var namestr = elempricestr.substring(0, pos-1);
 
-        for (var m =1; m<100; m++) {
+        for (var m =1; m<maxCount; m++) {
             price1 = 'price' + m;
             name1 = 'name' + m;
             var namedel = window.localStorage[name1];
             var pricedel = window.localStorage[price1];
             if (pricedel==elprice && namedel==namestr) {
-                localStorage.removeItem(namedel);
-                localStorage.removeItem(pricedel);
-                return;
+                localStorage.removeItem('name' + m);
+                localStorage.removeItem('price' + m);
+                break;
             }
         }
     }
